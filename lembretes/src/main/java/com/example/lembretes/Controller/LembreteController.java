1a14d8ec5e8b0bae33f6b588c412606fcd5e2af0
adailton90 <adailton.oliveira90@gmail.com>
2019-02-20 21:26:37 -0300

    Criando telaas de cadastro e lista de lembretes classe e metodos de controller de algumas requisições

--- lembretes/src/main/java/com/example/lembretes/Controller/LembreteController.java
+++ lembretes/src/main/java/com/example/lembretes/Controller/LembreteController.java
@@ -2,8 +2,12 @@ package com.example.lembretes.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.lembretes.Model.LembreteEntity;
 import com.example.lembretes.Repository.LembreteRepository;
 
 @Controller
@@ -17,5 +21,25 @@ public class LembreteController {
 		return "View/cadastroLembrete";
 	}
 	
-
+	
+	@GetMapping("/cadastrarLembrete")
+	public ModelAndView cadastrarLembrete() {
+		ModelAndView retorno = new ModelAndView("/View/cadastroLembrete");
+		retorno.addObject("lembrete", new LembreteEntity());
+		return retorno;
+	}
+	
+	@PostMapping
+	public ModelAndView cadastrarLembrete(LembreteEntity lembrete) {
+		repository.save(lembrete);
+		return new ModelAndView("redirect:/cadastrarLembrete");
+	}
+	
+	@GetMapping("/listarLembretes")
+	public ModelAndView listarLembretes() {
+		ModelAndView retorno = new ModelAndView("View/listaLembrete");
+		///retorno.addObject("listaLembrete", repository.findAll());//dando nome ao objeto e metodo de trazer todos 
+		return retorno;
+	}
+	
 }
